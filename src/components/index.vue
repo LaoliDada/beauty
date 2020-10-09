6fde6d2ba1f4529705de98788055a09ad1bc3b79
Liyi <liyi@qijiayoudao.com>
2020-10-09 22:58:19 +0800

    add filles

---
+++ src/components/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    123123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+   
+  }
+}
+</script>
---
+++ src/routers/index.js
@@ -0,0 +1,7 @@
+import home from "@/components/index";
+export default[
+    {
+        path:"/index",
+        component:home
+    }
+]
